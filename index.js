8ed45de3a856891a2c45fc3b03da8a26807b975d
jishnuvgopal <101096348+jishnuvgopal@users.noreply.github.com>
2022-08-31 11:52:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+    var typing=new Typed(".text", {
+      strings: [ "Web Designer", "Freelancer", "Graphics Designer", "Web Developer"],
+      typeSpeed: 100,
+      backSpeed: 40,
+      loop: true,
+    });
+   
